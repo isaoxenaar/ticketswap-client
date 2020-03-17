77ff5f6c0588cf64a0e5a27b258aaf6ca730c19c
Isa Oxenaar <isamoxenaar@gmail.com>
2020-03-17 23:31:33 +0100

    I added an eventcontainer details, like the max 9 per page, almost there

--- src/components/EventContainer.js
+++ src/components/EventContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { createEvent } from "../actions/createEventAction";
 import { getEvents } from "../actions/allEventsAction";
 import EventForm from "./EventForm";
@@ -40,17 +41,64 @@ class CreateEventContainer extends React.Component {
 
   render() {
     console.log("this is events", this.props.events);
-    console.log("this is jwt in eventform", this.props.events);
-    return (
-      <div>
-        <EventForm
-          onSubmit={this.onSubmit}
-          onChange={this.onChange}
-          values={this.state}
-        />
-        {}
-      </div>
-    );
+    console.log("this is jwt in eventform", this.props.loggedInUser);
+
+    const eventList = this.props.events.map(event => {
+      return (
+        <div>
+          <img
+            style={{ width: "200px", height: "200px" }}
+            src={event.pictureurl}
+            alt="not found"
+          />
+          <h3>{event.name}</h3>
+          <h4>{event.enddate}</h4>
+          <h5>{event.description}</h5>
+        </div>
+      );
+    });
+
+    const page = this.props.events.length / 9;
+
+    if (this.props.events.length <= 9) {
+      if (this.props.loggedInUser) {
+        return (
+          <main>
+            {" "}
+            <EventForm
+              onSubmit={this.onSubmit}
+              onChange={this.onChange}
+              values={this.state}
+            />
+            {eventList}
+          </main>
+        );
+      } else {
+        return <main>{eventList}</main>;
+      }
+    } else {
+      if (this.props.loggedInUser) {
+        return (
+          <main>
+            {" "}
+            <EventForm
+              onSubmit={this.onSubmit}
+              onChange={this.onChange}
+              values={this.state}
+            />
+            {eventList}
+            <Link to={`/events/${page}`}>more events</Link>
+          </main>
+        );
+      } else {
+        return (
+          <main>
+            {eventList}
+            <Link to={`/events/${page}`}>more events</Link>
+          </main>
+        );
+      }
+    }
   }
 }
 
